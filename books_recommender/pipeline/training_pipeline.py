0b48761fb970e0102f561025e93158f00cb3c0fc
vnithisha <vaddepallinithisha@gmail.com>
2026-09-16 14:52:44 +0530

    model trainer and data transformation are added

--- books_recommender/pipeline/training_pipeline.py
+++ books_recommender/pipeline/training_pipeline.py
@@ -1,15 +1,15 @@
 from books_recommender.components.stage_00_data_ingestion import DataIngestion
 from books_recommender.components.stage_01_data_validation import DataValidation
-# from books_recommender.components.stage_02_data_transformation import DataTransformation
-# from books_recommender.components.stage_03_model_trainer import ModelTrainer
+from books_recommender.components.stage_02_data_transformation import DataTransformation
+from books_recommender.components.stage_03_model_trainer import ModelTrainer
 
 
 class TrainingPipeline:
     def __init__(self):
         self.data_ingestion = DataIngestion()
         self.data_validation = DataValidation()
-        # self.data_transformation = DataTransformation()
-        # self.model_trainer = ModelTrainer()
+        self.data_transformation = DataTransformation()
+        self.model_trainer = ModelTrainer()
 
     def start_training_pipeline(self):
         """
@@ -18,5 +18,5 @@ class TrainingPipeline:
         """
         self.data_ingestion.initiate_data_ingestion()
         self.data_validation.initiate_data_validation()
-        # self.data_transformation.initiate_data_transformation()
-        # self.model_trainer.initiate_model_trainer()
+        self.data_transformation.initiate_data_transformation()
+        self.model_trainer.initiate_model_trainer()
